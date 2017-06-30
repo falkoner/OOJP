e052c2da7b3a89712828c8a6927c1c988152a685
Alex Filatau <filatau@gmail.com>
2017-06-29 23:04:06 -0700

    Added automatic sorting by centrality and retrieval of X top nodes

--- course_4/SocialNetwork/src/graph/SocialGraph.java
+++ course_4/SocialNetwork/src/graph/SocialGraph.java
@@ -1,19 +1,25 @@
 package graph;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by Alex Filatau
  * Modification of base CapGraph class enabled to work with following social graph features:
- * - degree centrality of nodes (number of connections a node has)
+ * - degree centralityMap of nodes (number of connections a node has)
  * - boolean state if the nodes (for state spread simulation)
  */
 public class SocialGraph extends CapGraph {
-    private HashMap<Integer, Integer> centrality = new HashMap<>();
+    private HashMap<Integer, Integer> centralityMap = new HashMap<>();
+    private TreeMap<Integer, List<Integer>> centralitySortedIndex = new TreeMap<>();
 
     /**
      * Add new edge to the graph
-     * Keep count on degree centrality of the nodes involved
+     * Keep count on degree centralityMap of the nodes involved
      *
      * @param from Integer value of the from vertex
      * @param to   Integer value of the to vertex
@@ -25,27 +31,57 @@ public class SocialGraph extends CapGraph {
     }
 
     /**
-     * Increase centrality count for the vertex
+     * Increase centralityMap count for the vertex
      *
      * @param vertex Integer value of the target vertex
      */
     private void incrementCentrality(int vertex) {
-        int centralityValue = this.centrality.getOrDefault(vertex, 0);
+        int centralityValue = this.centralityMap.getOrDefault(vertex, 0);
         centralityValue++;
-        this.centrality.put(vertex, centralityValue);
+        this.centralityMap.put(vertex, centralityValue);
+        List<Integer> vertexReferences = this.centralitySortedIndex.getOrDefault(centralityValue, new ArrayList<>());
+        vertexReferences.add(vertex);
+        this.centralitySortedIndex.put(centralityValue, vertexReferences);
     }
 
 
     /**
-     * Returns degree centrality value stored for the vertex
+     * Returns degree centralityMap value stored for the vertex
      *
      * @param vertex Integer value of the target vertex
-     * @return degree centrality
+     * @return degree centralityMap
      */
     public int getCentrality(int vertex) {
         if (!this.containsVertex(vertex)) {
             throw new IllegalArgumentException(String.format("Vertex [%s] is not in graph", vertex));
         }
-        return this.centrality.getOrDefault(vertex, 0);
+        return this.centralityMap.getOrDefault(vertex, 0);
+    }
+
+    /**
+     * Returns specified number (at most) of vertices sorted descending by centrality.
+     *
+     * @param numVertices number of vertices to return
+     * @return List of top vertices by centrality
+     */
+    public List<Integer> getTopVerticesByCentrality(int numVertices) {
+        List<Integer> topVertices = new ArrayList<>();
+        for (Integer centrality : this.centralitySortedIndex.keySet()) {
+            if (topVertices.size() >= numVertices) break;
+            List<Integer> vertices = this.centralitySortedIndex.get(centrality);
+            topVertices.addAll(vertices);
+        }
+
+        return topVertices.stream().limit(numVertices).collect(toList());
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("SocialGraph{");
+        sb.append("vertices=").append(this.getVertices());
+        sb.append(", edges=").append(this.getEdges());
+        sb.append(", centralitySortedIndex=").append(centralitySortedIndex);
+        sb.append('}');
+        return sb.toString();
     }
 }
--- course_4/SocialNetwork/src/graph/SocialGraphTest.java
+++ course_4/SocialNetwork/src/graph/SocialGraphTest.java
@@ -3,6 +3,8 @@ package graph;
 import org.junit.Test;
 import util.GraphLoader;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class SocialGraphTest {
@@ -41,12 +43,62 @@ public class SocialGraphTest {
     }
 
     @Test
+    public void vertexWithNoEdgesHas0Centrality() {
+        SocialGraph graph = new SocialGraph();
+        graph.addVertex(5);
+        assertEquals(0, graph.getCentrality(5));
+    }
+
+    @Test
     public void shouldBeAbleToLoadGraphFromFile() {
         SocialGraph graph = new SocialGraph();
         GraphLoader.loadGraph(graph, "data/small_test_graph.txt");
 
         assertTrue("Should contain vertex 14", graph.containsVertex(14));
         assertEquals(2, graph.getCentrality(14));
+    }
+
+    @Test
+    public void shouldBeAbleToGetTop3NodesByCentrality() {
+        SocialGraph graph = new SocialGraph();
+        graph.addVertex(0);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 1);
+        graph.addEdge(2, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 1);
+        graph.addVertex(4);
+
+        List<Integer> result = graph.getTopVerticesByCentrality(3);
+        assertEquals(3, result.size());
+        assertEquals((Integer) 1, result.get(0));
+        assertEquals((Integer) 2, result.get(1));
+        assertEquals((Integer) 3, result.get(2));
+    }
+
+    @Test
+    public void noTopNodesByCentralityOnEmptyGraph() {
+        SocialGraph graph = new SocialGraph();
+        assertEquals(0, graph.getTopVerticesByCentrality(1).size());
+    }
+
+    @Test
+    public void manyTopNodesByCentalityOnOneVertexGraph() {
+        SocialGraph graph = new SocialGraph();
+        graph.addEdge(1, 2);
+        List<Integer> result = graph.getTopVerticesByCentrality(10);
+        assertEquals(1, result.size());
+        assertEquals((Integer) 1, result.get(0));
+    }
+
+    @Test
+    public void toStringShouldShowCentrality() {
+        SocialGraph graph = new SocialGraph();
+        graph.addEdge(1, 2);
+        String expected = "SocialGraph{vertices=[1, 2], edges={1=[2]}, centralitySortedIndex={1=[1]}}";
+        assertEquals(expected, graph.toString());
     }
 
 }
