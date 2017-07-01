53f5df2c9f42f8381da43b19c2c895fc5d40c776
Alex Filatau <filatau@gmail.com>
2017-07-01 00:20:09 -0700

    Initial implementation of Spread modeling algorithm - interface and SocialSpread abstract class

---
+++ course_4/SocialNetwork/src/graph/SocialSpread.java
@@ -0,0 +1,124 @@
+package graph;
+
+import java.util.*;
+
+/**
+ * Created by Alex Filatau.
+ * Abstract generic spread common for all implementation options
+ */
+abstract class SocialSpread implements Spread {
+
+    private final SocialGraph graph;
+    private int currentStepNumber;
+    private Set<Integer> triggeredVertices;
+    private Set<Integer> lastTriggeredVertices;
+
+    /**
+     * Any social spread requires target graph to work on
+     *
+     * @param graph target Graph
+     */
+    public SocialSpread(SocialGraph graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    public int getCurrentStepNumber() {
+        return this.currentStepNumber;
+    }
+
+    @Override
+    public int getNumberOfTriggeredVertices() {
+        return this.triggeredVertices.size();
+    }
+
+    /**
+     * Are there any adjusting vertices that might be affected by next step
+     *
+     * @return
+     */
+    @Override
+    public boolean isSpreadable() {
+        if (isCompleted()) return false;
+        if (this.graph.getVertices().isEmpty()) return false;
+        for (Integer vertex : this.graph.getVertices()) {
+            if (isVertexTriggered(vertex)) continue;
+            if (!getNonTriggeredFriends(vertex).isEmpty()) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns List of friends of specified vertex which are not marked as triggered
+     *
+     * @param vertex target vertex
+     * @return List of vertices
+     */
+    public List<Integer> getNonTriggeredFriends(Integer vertex) {
+        HashSet<Integer> friends = this.graph.getNeighbors(vertex);
+
+        if (friends.isEmpty()) return Collections.emptyList();
+
+        List<Integer> result = new LinkedList<>();
+        for (Integer friend : friends) {
+            if (!isVertexTriggered(friend)) result.add(friend);
+        }
+
+        return result;
+
+    }
+
+    /**
+     * Check if specified vertex is marked as triggered
+     * Throws
+     *
+     * @param vertex vertex to check
+     * @return True if vertex marked as triggered
+     */
+    public boolean isVertexTriggered(int vertex) {
+        if (!this.graph.containsVertex(vertex))
+            throw new IllegalArgumentException(String.format("Requested vertex [%s]is not in graph", vertex));
+        return this.getAllTriggeredVertices().contains(vertex);
+    }
+
+    /**
+     * Spread is complete if all vertices are triggered
+     *
+     * @return True if completed
+     */
+    @Override
+    public boolean isCompleted() {
+        return getAllTriggeredVertices().size() == this.graph.getVertices().size();
+    }
+
+    /**
+     * Get Set of all vertices that were affected by previous steps
+     *
+     * @return Set of vertices
+     */
+    @Override
+    public Set<Integer> getAllTriggeredVertices() {
+        return this.triggeredVertices;
+    }
+
+    /**
+     * Get Set of vertices that were affected by last step
+     *
+     * @return Set of vertices
+     */
+    @Override
+    public Set<Integer> getLastTriggeredVertices() {
+        return this.lastTriggeredVertices;
+    }
+
+    /**
+     * Resets state of the spread to initial state
+     */
+    @Override
+    public void reset() {
+        this.currentStepNumber = 0;
+        this.triggeredVertices = new HashSet<>();
+        this.lastTriggeredVertices = new HashSet<>();
+    }
+
+}
---
+++ course_4/SocialNetwork/src/graph/Spread.java
@@ -0,0 +1,56 @@
+package graph;
+
+import java.util.Set;
+
+/**
+ * Created by Alex Filatau
+ * Interface of some generic spread simulation (infection, idea etc) through
+ * a social network. Allows multiple implementation options - absolute, conditional, probabilistic etc
+ */
+public interface Spread {
+
+    /**
+     * Process next generation of spread
+     *
+     * @return step number
+     */
+    int step();
+
+    int getCurrentStepNumber();
+
+    int getNumberOfTriggeredVertices();
+
+    /**
+     * Are there any adjusting vertices that might be affected by next step
+     *
+     * @return
+     */
+    boolean isSpreadable();
+
+    /**
+     * Spread is complete if all vertices are triggered
+     *
+     * @return True if completed
+     */
+    boolean isCompleted();
+
+    /**
+     * Get Set of all vertices that were affected by previous steps
+     *
+     * @return Set of vertices
+     */
+    Set<Integer> getAllTriggeredVertices();
+
+    /**
+     * Get Set of vertices that were affected by last step
+     *
+     * @return Set of vertices
+     */
+    Set<Integer> getLastTriggeredVertices();
+
+    /**
+     * Resets state of the spread to initial state
+     */
+    void reset();
+
+}
